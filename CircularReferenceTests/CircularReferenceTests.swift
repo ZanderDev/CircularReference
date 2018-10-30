c652fad1b36243b166e64cdb01d3d3dbe94cd533
alphaZ <zhangdong15@jd.com>
2018-10-30 09:46:28 +0800

    first cm

---
+++ CircularReferenceTests/CircularReferenceTests.swift
@@ -0,0 +1,34 @@
+//
+//  CircularReferenceTests.swift
+//  CircularReferenceTests
+//
+//  Created by AlphaZ on 2018/10/30.
+//  Copyright © 2018年 Zhang,Dong(AI). All rights reserved.
+//
+
+import XCTest
+@testable import CircularReference
+
+class CircularReferenceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
